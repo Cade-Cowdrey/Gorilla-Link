af80ff01d0cae0129636ab6c69951db3975c33d3
Connor Vandenberg <connorvandenberg06@gmail.com>
2025-10-21 23:41:07 -0500

    Update routes.py

--- blueprints/auth/routes.py
+++ blueprints/auth/routes.py
@@ -1,10 +1,15 @@
-from flask import Blueprint, render_template
-bp = Blueprint("auth_bp", __name__, url_prefix="/auth")
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 
-@bp.get("/login")
+auth_bp = Blueprint("auth_bp", __name__, template_folder="../../templates")
+
+@auth_bp.route("/login")
 def login():
     return render_template("auth/login.html")
 
-@bp.get("/register")
+@auth_bp.route("/register")
 def register():
     return render_template("auth/register.html")
+
+@auth_bp.route("/forgot")
+def forgot():
+    return render_template("auth/forgot.html")
