9f015abf99e02035fe61d7343f98b2307167d7fb
Connor Vandenberg <connorvandenberg06@gmail.com>
2025-10-21 23:44:11 -0500

    Update routes.py

--- blueprints/donor/routes.py
+++ blueprints/donor/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
-bp = Blueprint("donor_bp", __name__, url_prefix="/donor")
 
-@bp.get("/portal")
+donor_bp = Blueprint("donor_bp", __name__, template_folder="../../templates")
+
+@donor_bp.route("/")
 def portal():
     return render_template("donor/portal.html")
