62fccc85bd89d71abe8f121fd889441fe7eeb176
Connor Vandenberg <connorvandenberg06@gmail.com>
2025-10-22 01:00:22 -0500

    Update __init__.py

--- blueprints/alumni/__init__.py
+++ blueprints/alumni/__init__.py
@@ -1,1 +1,8 @@
-# alumni package
+# ============================================================
+# FILE: blueprints/alumni/__init__.py
+# ============================================================
+from flask import Blueprint
+
+alumni_bp = Blueprint("alumni_bp", __name__, url_prefix="/alumni", template_folder="../../templates/alumni")
+
+from . import routes  # noqa: E402,F401
