021784719ccde85dfc9ca27129ae0a67d7ac9acf
Connor Vandenberg <connorvandenberg06@gmail.com>
2025-10-22 01:01:53 -0500

    Update __init__.py

--- blueprints/emails/__init__.py
+++ blueprints/emails/__init__.py
@@ -1,1 +1,8 @@
-# emails package
+# ============================================================
+# FILE: blueprints/emails/__init__.py
+# ============================================================
+from flask import Blueprint
+
+emails_bp = Blueprint("emails_bp", __name__, url_prefix="/emails", template_folder="../../templates/emails")
+
+from . import routes  # noqa: E402,F401
