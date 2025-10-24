856fd21c1cbaeba6080f0b971f64c4bfc8a8a95e
Connor Vandenberg <connorvandenberg06@gmail.com>
2025-10-23 22:06:29 -0500

    Update __init__.py

--- blueprints/donor/__init__.py
+++ blueprints/donor/__init__.py
@@ -1,8 +1,1 @@
-# ============================================================
-# FILE: blueprints/donor/__init__.py
-# ============================================================
-from flask import Blueprint
-
-donor_bp = Blueprint("donor_bp", __name__, url_prefix="/donor", template_folder="../../templates/donor")
-
-from . import routes  # noqa: E402,F401
+from .routes import donor_bp
