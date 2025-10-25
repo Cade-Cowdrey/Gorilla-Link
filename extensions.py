0481cd3e3e27b16a37c59cd6664cf16eea4ad3b8
Connor Vandenberg <connorvandenberg06@gmail.com>
2025-10-25 03:56:41 -0500

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,11 +1,17 @@
+# extensions.py
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_mail import Mail
 from flask_caching import Cache
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_mail import Mail
+from flask_migrate import Migrate
+from flask_socketio import SocketIO
 
 db = SQLAlchemy()
-migrate = Migrate()
 login_manager = LoginManager()
+cache = Cache()
+limiter = Limiter(key_func=get_remote_address)
 mail = Mail()
-cache = Cache(config={"CACHE_TYPE": "SimpleCache"})
+migrate = Migrate()
+socketio = SocketIO(async_mode="threading")  # eventlet is installed; switch if desired
