9e96d8a5b48430a3a0a47a51caf015d24816581a
Connor Vandenberg <connorvandenberg06@gmail.com>
2025-10-31 08:59:11 -0700

    Refactor departments blueprint: rename to departments_bp and use record_page_view

--- blueprints/departments/routes.py
+++ blueprints/departments/routes.py
@@ -1,11 +1,13 @@
+# blueprints/departments/routes.py
 from flask import Blueprint, render_template
 from utils.security_util import login_required_safe
-from utils.analytics_util import track_page_view
+from utils.analytics_util import record_page_view
 
-bp = Blueprint("departments", __name__, url_prefix="/departments")
+bp = Blueprint("departments_bp", __name__, url_prefix="/departments")
 
 @bp.route("/")
 @login_required_safe
 def department_index():
-    track_page_view("departments_index")
+    """Display a list of departments."""
+    record_page_view("departments_index")
     return render_template("departments/index.html", title="Departments | PittState-Connect")
